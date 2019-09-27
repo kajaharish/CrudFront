e3606e29950e621f347a2d487fa73ff5c203b532
kajaharish <kajaharish4hud@gmail.com>
2019-09-27 10:02:27 +0530

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EnterDetailComponent} from "./enter-detail/enter-detail.component";
+import {ShowDetailComponent} from "./show-detail/show-detail.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'insert',component: EnterDetailComponent},
+  {path: 'fetch', component: ShowDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnterDetailComponent } from './enter-detail/enter-detail.component';
+import { ShowDetailComponent } from './show-detail/show-detail.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnterDetailComponent,
+    ShowDetailComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
