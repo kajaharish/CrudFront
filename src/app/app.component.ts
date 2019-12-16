a94a7c952bfbcd558b6913c14c7b3ebe4ff7da17
kajaharish <kajaharish4hud@gmail.com>
2019-12-16 14:20:51 +0530

    changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {FormService} from "./form.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'Crud ';
+  title = 'Crud operation..';
 
   flag:boolean;
   flag2:boolean;
